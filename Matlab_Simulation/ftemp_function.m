dacb774d0a47ad3bc022673dbdae66bff665c78a
Maithilishetty <35223503+Maithilishetty@users.noreply.github.com>
2019-10-11 14:44:41 +0530

    Update ftemp_function.m

--- Matlab_Simulation/ftemp_function.m
+++ Matlab_Simulation/ftemp_function.m
@@ -10,13 +10,13 @@ global Idstd Tstd beta
     Vgs = ftemp_inputs(2);
     Vds = ftemp_inputs(3);
     
-    if (Vgs < Vth)
-        Idstd = 0;
-    elseif ((Vds > 0) && (Vds < ((Vgs - Vth)/(1+a))))
-        Idstd = k * (((Vgs-Vth)*Vds) - (((1+a)*Vds^2)/2))*(1+lambda*Vds);
-    elseif ((Vds > (Vgs-Vth)/(1+a)) &&  ((Vgs-Vth)/(1+a) > 0))
-        Idstd = (k / 2*(1+a)) * (1+lambda*Vds) * (Vgs - Vth)^2;
-    end
+%     if (Vgs < Vth)
+%         Idstd = 0;
+%     elseif ((Vds > 0) && (Vds < ((Vgs - Vth)/(1+a))))
+%         Idstd = k * (((Vgs-Vth)*Vds) - (((1+a)*Vds^2)/2))*(1+lambda*Vds);
+%     elseif ((Vds > (Vgs-Vth)/(1+a)) &&  ((Vgs-Vth)/(1+a) > 0))
+%         Idstd = (k / 2*(1+a)) * (1+lambda*Vds) * (Vgs - Vth)^2;
+%     end
     
     ftemp = Idstd*(((T/Tstd)^beta) - ((T/Tstd)^(-1.5)));
 end  
